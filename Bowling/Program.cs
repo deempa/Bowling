8c07451381a1857560d53a3f219d8ef91d28c1cd
deempa <62642034+deempa@users.noreply.github.com>
2021-02-18 20:26:48 +0200

    Delete unused files

--- Bowling/Program.cs
+++ Bowling/Program.cs
@@ -7,6 +7,8 @@ namespace Bowling
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Welcome To The Bowling");
             var game = new Game();
             game.RollBowling(10, 0);
             game.RollBowling(10, 0);
